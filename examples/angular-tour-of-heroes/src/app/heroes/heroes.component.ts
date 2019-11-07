6648f3b964f41c666f4738ab709b1367e4c7d400
Rob Brennan <rob@therobbrennan.com>
2019-11-07 15:50:10 -0800

    Added the ability to delete a hero

--- examples/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ examples/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -26,6 +26,23 @@ export class HeroesComponent implements OnInit {
     });
   }
 
+  delete(hero: Hero): void {
+    /**
+     * Although the component delegates hero deletion to the HeroService, it
+     * remains responsible for updating its own list of heroes. The component's
+     * delete() method immediately removes the hero-to-delete from that list,
+     * anticipating that the HeroService will succeed on the server.
+     */
+    this.heroes = this.heroes.filter(h => h !== hero);
+
+    // There's really nothing for the component to do with the Observable
+    // returned by heroService.delete() but it must subscribe anyway.
+    //
+    // If you neglect to subscribe(), the service will not send the
+    // delete request to the server. As a rule, an Observable does nothing until something subscribes.
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
