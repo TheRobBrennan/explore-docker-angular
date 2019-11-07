e11d73a149e7839e9e59628dbca02b3792230b4b
Rob Brennan <rob@therobbrennan.com>
2019-11-07 14:25:39 -0800

    Generated a new hero service

---
+++ examples/angular-tour-of-heroes/src/app/hero.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable({
+  // When you provide the service at the root level, Angular creates a single,
+  // shared instance of HeroService and injects into any class that asks for it.
+  //
+  // Registering the provider in the @Injectable metadata also allows Angular
+  // to optimize an app by removing the service if it turns out not to be used after all.
+  providedIn: 'root',
+})
+export class HeroService {
+  constructor() {}
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
+}
