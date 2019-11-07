fb6dc0fa72a9a31df14f81458440c8ac76851483
Rob Brennan <rob@therobbrennan.com>
2019-11-07 15:45:39 -0800

    Added ability to add a hero

--- examples/angular-tour-of-heroes/src/app/hero.service.ts
+++ examples/angular-tour-of-heroes/src/app/hero.service.ts
@@ -18,13 +18,21 @@ import { MessageService } from './message.service';
 export class HeroService {
   private heroesUrl = 'api/heroes'; // URL to web api
   httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
 
   constructor(
     private http: HttpClient,
     private messageService: MessageService
   ) {}
+
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
 
   /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
