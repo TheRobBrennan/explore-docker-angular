01f02a39e2a90b21cb4bbd2c6ed9771fbacf2c18
Rob Brennan <rob@therobbrennan.com>
2019-11-07 14:58:30 -0800

    Added path to display Dashboard component

--- examples/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ examples/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -8,8 +8,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
 
 // Initializes the router and starts it listening for browser location changes
 @NgModule({
