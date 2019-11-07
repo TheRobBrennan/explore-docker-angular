079d0873a764c8db3611ba10b0464be42baefcac
Rob Brennan <rob@therobbrennan.com>
2019-11-07 15:46:04 -0800

    Add ability to add a hero with a controlled input field

--- examples/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ examples/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -10,14 +10,23 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.getHeroes();
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({ name } as Hero).subscribe(hero => {
+      this.heroes.push(hero);
+    });
+  }
+
   getHeroes(): void {
-    this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
 }
