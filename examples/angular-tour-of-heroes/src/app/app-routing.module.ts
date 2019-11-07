5fe0020fdd4e953d434c33c808b35d4236b6ff3b
Rob Brennan <rob@therobbrennan.com>
2019-11-07 14:51:21 -0800

    Added reference comments to routing example

--- examples/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ examples/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -11,8 +11,15 @@ import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
 
+// Initializes the router and starts it listening for browser location changes
 @NgModule({
+  /**
+   * The method is called forRoot() because you configure the router at the application's
+   * root level. The forRoot() method supplies the service providers and directives needed
+   * for routing, and performs the initial navigation based on the current browser URL.
+   */
   imports: [RouterModule.forRoot(routes)],
+  // Export RouterModule so it will be available throughout the app
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
