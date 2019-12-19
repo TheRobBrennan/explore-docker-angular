d98dc4ceec5a3fb0b5c4ed2092356f9b8e64f499
Rob Brennan <rob@therobbrennan.com>
2019-12-19 13:08:38 -0800

    ng g c product-get --skipTests=true

--- examples/javatpoint-angular-8-tutorial/app/src/app/app.module.ts
+++ examples/javatpoint-angular-8-tutorial/app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductGetComponent } from './product-get/product-get.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductAddComponent
+    ProductAddComponent,
+    ProductGetComponent
   ],
   imports: [
     BrowserModule,
