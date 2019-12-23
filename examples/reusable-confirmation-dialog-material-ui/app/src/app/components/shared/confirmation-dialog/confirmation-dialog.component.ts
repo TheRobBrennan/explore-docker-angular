a733005d99ffa8d15c07e5b1c03788719a0b1554
Rob Brennan <rob@therobbrennan.com>
2019-12-23 13:57:49 -0800

    Initial implementation of confirmation-dialog

--- examples/reusable-confirmation-dialog-material-ui/app/src/app/components/shared/confirmation-dialog/confirmation-dialog.component.ts
+++ examples/reusable-confirmation-dialog-material-ui/app/src/app/components/shared/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-confirmation-dialog',
   templateUrl: './confirmation-dialog.component.html',
   styleUrls: ['./confirmation-dialog.component.css']
 })
-export class ConfirmationDialogComponent implements OnInit {
+export class ConfirmationDialogComponent {
+  constructor(
+    /* Receive a reference to the dialog we are currently opening */
+    public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
+    /* Inject the message to be displayed on this dialog */
+    @Inject(MAT_DIALOG_DATA) public message: string) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  onNoClick(): void {
+    this.dialogRef.close();
   }
-
 }
