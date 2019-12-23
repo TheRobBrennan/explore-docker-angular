92df6b4c36ce4d673ff3ae739224e9373daba152
Rob Brennan <rob@therobbrennan.com>
2019-12-23 14:27:17 -0800

    Minor improvement to the E2E describe statement

--- examples/reusable-confirmation-dialog-material-ui/app/e2e/src/app.e2e-spec.ts
+++ examples/reusable-confirmation-dialog-material-ui/app/e2e/src/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('reusable-confirmation-dialog-material-ui App', () => {
   let page: AppPage;
 
   beforeEach(() => {
