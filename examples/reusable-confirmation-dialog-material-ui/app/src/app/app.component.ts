90e7cc17c90af311b7908d40472b866d4be7f25e
Rob Brennan <rob@therobbrennan.com>
2019-12-23 14:02:20 -0800

    App component now allows user to click on a button to launch our reusable confirmation dialog component

--- examples/reusable-confirmation-dialog-material-ui/app/src/app/app.component.ts
+++ examples/reusable-confirmation-dialog-material-ui/app/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { MatDialog } from '@angular/material';
+import { ConfirmationDialogComponent } from './components/shared/confirmation-dialog/confirmation-dialog.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'angular-confirmation-dialog';
+  constructor(public dialog: MatDialog) {}
+  openDialog(): void {
+    const dialogConfig = {
+      width: '350px',
+      data: 'Do you confirm the deletion of this data?'
+    };
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, dialogConfig);
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log('Yes clicked');
+        // DO SOMETHING
+      }
+    });
+  }
 }
