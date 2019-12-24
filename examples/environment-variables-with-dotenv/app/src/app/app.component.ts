c218244ac70e56b9a9fea85ce56cdea85b13e1db
Rob Brennan <rob@therobbrennan.com>
2019-12-23 16:29:27 -0800

    Initial implementation of our app component

--- examples/environment-variables-with-dotenv/app/src/app/app.component.ts
+++ examples/environment-variables-with-dotenv/app/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConfigService } from './services/config/config.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public message;
+
+  config = this.configService.getConfig();
+
+  constructor(private configService: ConfigService) {}
+
+  ngOnInit() {
+    this.message = `
+      Your API environment variable is: ${this.config.API}
+      Your TEST environment variable is: ${this.config.TEST}
+    `;
+  }
 }
